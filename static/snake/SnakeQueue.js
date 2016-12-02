28206e8650716a7416f419f55bb25ecd6d20d28b
amir.mirsalimi <amir.mirsalimi@workday.com>
2016-12-02 03:02:06 -0800

    added queue limit

--- static/snake/SnakeQueue.js
+++ static/snake/SnakeQueue.js
@@ -15,9 +15,11 @@ class SnakeQueue{
   checkQueue(){
     if(this.delay <= 0){
       if(this.players.length > 0){
-        let player = this.players.pop();
-        Snake.addNewSnake(player);
-        this.delay= 25;
+        if(uiIds.length != 0 ){
+          let player = this.players.pop();
+          Snake.addNewSnake(player);
+          this.delay= 25;
+        }
       }
     } else if(this.delay > 0) {
       this.delay--;
