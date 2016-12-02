cb98f9979dc6c25a70fcaf5b0e6efb85d449215d
justinhorton <justinkhorton@gmail.com>
2016-12-01 23:57:49 -0800

    Map various events to direction string

--- static/js/client.js
+++ static/js/client.js
@@ -1,18 +1,38 @@
 const buttonClickEventNames = ["mousedown", "touchstart"];
-const arrowKeycodes = [37, 38, 39, 40, 87, 65, 83, 68] // left, up, right, down, w, a, s, d
+const eventMap = new Map();
 
+initializeEventMap();
 initializeAllListeners();
 // TODO: what is the server ID? what about client ID?
 //const socket = new WebSocket(getWebSocketBase() + 'ws?type=client&serverId=server_1');
 
+function initializeEventMap() {
+  eventMap.set(37, "left");
+  eventMap.set(65, "left");
+  eventMap.set("buttonLeft", "left");
+
+  eventMap.set(38, "up");
+  eventMap.set(87, "up");
+  eventMap.set("buttonUp", "up");
+
+  eventMap.set(39, "right");
+  eventMap.set(68, "right");
+  eventMap.set("buttonRight", "right");
+
+  eventMap.set(40, "down");
+  eventMap.set(83, "down");
+  eventMap.set("buttonDown", "down");
+}
 
 // initialize button listeners for click, click-like, and keypress events
 function initializeAllListeners() {
   // listen to arrow keys and wasd
   document.addEventListener("keydown", (event) => {
-      if (arrowKeycodes.includes(event.keyCode)) {
+      //if (arrowKeycodes.includes(event.keyCode)) {
+      if (eventMap.has(event.keyCode)) {
         event.preventDefault();
-        sendThingToTheSocketThing("key:" + event.keyCode);
+        console.log("key:" + event.keyCode);
+        sendThingToTheSocketThing(eventMap.get(event.keyCode));
       }
   });
 
@@ -21,14 +41,15 @@ function initializeAllListeners() {
   for (let button of buttons) {
     for (eventName of buttonClickEventNames) {
       button.addEventListener(eventName, (event) => {
-          sendThingToTheSocketThing(button.id);
+          console.log("button:" + button.id);
+          sendThingToTheSocketThing(eventMap.get(button.id));
       });
     }
   }
 }
 
 function sendThingToTheSocketThing(message) {
-  console.log(message);
+  console.log("message contents:" + message);
   // TODO: compose the thing into the proper format and send it
   // socket.send(message);
 }
