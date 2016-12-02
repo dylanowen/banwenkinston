214480a681a3c5af4337bd82a85beb83ed686b0d
justin.horton <justin.horton@workday.com>
2016-12-01 19:45:15 -0800

    Change client to use div buttons, add listener skeleton
    Still need to open a websocket and send the commands.

--- static/js/client.js
+++ static/js/client.js
@@ -1,120 +1,24 @@
-document.getElementById("keyLeft").addEventListener("click", function(){ document.getElementById("demo").innerHTML="changed"; });
+var buttonClickEventNames = ["mousedown", "touchstart"];
 
+initializeAllButtonListeners();
 
-
-  var airconsole = new AirConsole();
-
-  var dpad = new DPad("dpad-0", {
-    relative: true,
-    distance: { x: 10, y: 10},
-    "directionchange": function(key, pressed) {
-      airconsole.message(AirConsole.SCREEN, {
-        "dpad0": {
-          "directionchange": {
-            "key": key,
-            "pressed": pressed
-          }
-        }
-      });
-    },
-    "touchstart": function() {
-      airconsole.message(AirConsole.SCREEN, {
-        "dpad0": {
-          "touch": true
-        }
-      });
-    },
-    "touchend": function(had_direction) {
-      airconsole.message(AirConsole.SCREEN, {
-        "dpad0": {
-          "touch": false,
-          "had_direction": had_direction
-        }
-      });
+// initialize button listeners for click and click-like events
+function initializeAllButtonListeners() {
+  var buttons = document.getElementsByClassName("buttondiv");
+  for (button of buttons) {
+    for (eventName of buttonClickEventNames) {
+      registerButtonListener(button);
     }
+  }
+}
+
+// register a listener that sends the button press to the web socket
+function registerButtonListener(button) {
+  button.addEventListener(eventName, (event) => {
+    sendThingToTheSocketThing(button.id);
   });
-  var dpad1 = new DPad("dpad-1", {
-    relative: true,
-    distance: { x: 10, y: 10},
-    "directionchange": function(key, pressed) {
-      airconsole.message(AirConsole.SCREEN, {
-        "dpad1": {
-          "directionchange": {
-            "key": key,
-            "pressed": pressed
-          }
-        }
-      });
-    },
-    "touchstart": function() {
-      airconsole.message(AirConsole.SCREEN, {
-        "dpad1": {
-          "touch": true
-        }
-      });
-    },
-    "touchend": function(had_direction) {
-      airconsole.message(AirConsole.SCREEN, {
-        "dpad1": {
-          "touch": false,
-          "had_direction": had_direction
-        }
-      });
-    },
-    "diagonal": true
-  });
-  var dpad2 = new DPad("dpad-2", {
-    "directionchange": function(key, pressed) {
-      airconsole.message(AirConsole.SCREEN, {
-        "dpad2": {
-          "directionchange": {
-            "key": key,
-            "pressed": pressed
-          }
-        }
-      });
-    },
-    "touchstart": function() {
-      airconsole.message(AirConsole.SCREEN, {
-        "dpad2": {
-          "touch": true
-        }
-      });
-    },
-    "touchend": function(had_direction) {
-      airconsole.message(AirConsole.SCREEN, {
-        "dpad2": {
-          "touch": false,
-          "had_direction": had_direction
-        }
-      });
-    }
-  });
-  var dpad3 = new DPad("dpad-3", {
-    diagonal: true,
-    "directionchange": function(key, pressed) {
-      airconsole.message(AirConsole.SCREEN, {
-        "dpad3": {
-          "directionchange": {
-            "key": key,
-            "pressed": pressed
-          }
-        }
-      });
-    },
-    "touchstart": function() {
-      airconsole.message(AirConsole.SCREEN, {
-        "dpad3": {
-          "touch": true
-        }
-      });
-    },
-    "touchend": function(had_direction) {
-      airconsole.message(AirConsole.SCREEN, {
-        "dpad3": {
-          "touch": false,
-          "had_direction": had_direction
-        }
-      });
-    }
-  });
+}
+
+function sendThingToTheSocketThing(string) {
+  console.log(string);
+}
